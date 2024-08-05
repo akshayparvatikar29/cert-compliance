f0276c32f8126afc9ef836bce0e2e0e71e47e866
akshayparvatikar29 <167433616+akshayparvatikar29@users.noreply.github.com>
2024-08-05 09:13:26 +0530

    Delete controls/default.rb

--- controls/default.rb
+++
@@ -1,8 +0,0 @@
-control 'CHECK github.com' do
-  impact 0.7
-  title 'Test control'
-  describe ssl_certificate(host: 'github.com', port: 443) do
-    it { should exist }
-    its('expiration_days') { should be >= 30 }
-  end
-end
