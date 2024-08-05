53672699c26cb93e03a3a826060490c254a7fddf
akshayparvatikar29 <167433616+akshayparvatikar29@users.noreply.github.com>
2024-08-05 09:35:31 +0530

    Delete controls/default.rb

--- controls/default.rb
+++
@@ -1,8 +0,0 @@
-control 'cert-check' do
-  impact 1.0
-  title 'Check Certificate'
-  desc 'Check for the version'
-  describe x509_certificate(filepath: '/git-repo/compliance/profiles/cert/controls/certificate.crt') do
-    its('validity_in_days') { should be > 30 }
-  end
-end
