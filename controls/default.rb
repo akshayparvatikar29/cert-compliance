a245122872491bf20af7ce05a7b555bf47707a7f
akshayparvatikar29 <167433616+akshayparvatikar29@users.noreply.github.com>
2024-08-05 09:59:02 +0530

    Add files via upload

---
+++ controls/default.rb
@@ -0,0 +1,8 @@
+control 'cert-check' do
+  impact 1.0
+  title 'Check Certificate'
+  desc 'Check for the version'
+  describe x509_certificate(filepath: 'home/ec2-user/git-repo/compliance/profiles/cert/controls/certificate.crt') do
+    its('validity_in_days') { should be > 30 }
+  end
+end
